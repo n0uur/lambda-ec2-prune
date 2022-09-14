3adcd05bd54412c2879386abae8761b94ff3b71d
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2022-09-14 18:07:01 +0700

    simplify handler and delete unused files

--- handler.py
+++ handler.py
@@ -1,16 +1,12 @@
 import sys
-import os
 
 sys.path.insert(0, 'vendor')
-
-from dotenv import load_dotenv
 import boto3
-load_dotenv()
 
 def stop(event, context):
     ec2 = boto3.resource('ec2',
-        aws_access_key_id=os.getenv('EC2_ACCESS_KEY'),
-        aws_secret_access_key=os.getenv('EC2_SECRET_KEY'),
+        aws_access_key_id="<ACCESS_KEY>",
+        aws_secret_access_key="<SECRET_KEY>",
     )
     filters = [
         {
@@ -34,5 +30,4 @@ def stop(event, context):
         "status": 200,
         "running_instances": instances_count,
         "stopped_instances": stopped_instances_count,
-        "ref": os.getenv('EC2_ACCESS_KEY', "NILACCESSKEY")[:4] + "***********"
     }
--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-pip install -t vendor -r requirements.txt
